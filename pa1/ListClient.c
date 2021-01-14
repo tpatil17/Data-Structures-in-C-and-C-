7f99fd785d94e3ec35f071f8463d7c312e77d0c6
Tanishq Patil <tmpatil@ucsc.edu>
2021-01-14 04:44:16 -0800

    Upload New File listClient.c

---
+++ pa1/ListClient.c
@@ -0,0 +1,59 @@
+/****************************************************************************************
+*  ListClient.c
+*  Test client for List ADT
+*****************************************************************************************/
+#include<stdio.h>
+#include<stdlib.h>
+#include"List.h"
+
+int main(int argc, char* argv[]){
+   
+   List A = newList();
+   List B = newList();
+   List C = NULL;
+   int i;
+
+   for(i=1; i<=20; i++){
+      append(A,i);
+      prepend(B,i);
+   }
+
+   printList(stdout,A); 
+   printf("\n");
+   printList(stdout,B); 
+   printf("\n");
+
+   for(moveFront(A); index(A)>=0; moveNext(A)){
+      printf("%d ", get(A));
+   }
+   printf("\n");
+   for(moveBack(B); index(B)>=0; movePrev(B)){
+      printf("%d ", get(B));
+   }
+   printf("\n");
+
+   C = copyList(A);
+   printf("%s\n", equals(A,B)?"true":"false");
+   printf("%s\n", equals(B,C)?"true":"false");
+   printf("%s\n", equals(C,A)?"true":"false");
+
+
+   moveFront(A);
+   for(i=0; i<5; i++) moveNext(A); // at index 5
+   insertBefore(A, -1);            // at index 6
+   for(i=0; i<9; i++) moveNext(A); // at index 15
+   insertAfter(A, -2);
+   for(i=0; i<5; i++) movePrev(A); // at index 10
+   delete(A);
+   printList(stdout,A);
+   printf("\n");
+   printf("%d\n", length(A));
+   clear(A);
+   printf("%d\n", length(A));
+
+   freeList(&A);
+   freeList(&B);
+   freeList(&C);
+
+   return(0);
+}
