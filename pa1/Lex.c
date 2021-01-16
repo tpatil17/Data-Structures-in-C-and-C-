575d88b093220903687e50004659dba3a42b6f35
Tanishq Patil <tmpatil@ucsc.edu>
2021-01-16 11:17:07 -0800

    The Lex.c works on txt files

--- pa1/Lex.c
+++ pa1/Lex.c
@@ -4,14 +4,12 @@
 #include "List.h"
 
 
-int main(int argc, char * argv[]){
+int main(int argc, char *argv[]){
 
-    int line_count = 0;
-    char word[300];
+    int line_count =0;
     char ch;
-    FILE *in_ct =NULL;
-    FILE *in_read = NULL;
-    FILE *out = NULL;
+    FILE *in;
+    FILE *out;
 
 
     // check command line for correct number of arguments
@@ -20,26 +18,19 @@ int main(int argc, char * argv[]){
         exit(1);
     }
     // open files for reading and writing 
-   in_ct = fopen(argv[1], "r");
-      if( in_ct==NULL ){
-      printf("Unable to open file %s for reading\n", argv[1]);
+   in = fopen(argv[1], "r");
+      if( in==NULL ){
+      printf(" 1 Unable to open file %s for reading\n", argv[1]);
       exit(1);
    }
     // count the number of lines
-    while((ch=fgetc(in_ct))!=EOF) {
+    while((ch=fgetc(in))!=EOF) {
         if(ch=='\n'){
+		
             line_count++;
         }
-    }
-    // Close the file
-    fclose(in_ct);
-
-    // Re open to read
-    in_read = fopen(argv[1], "r");
-      if( in_read==NULL ){
-      printf("Unable to open file %s for reading\n", argv[1]);
-      exit(1);
-      }
+  }
+ rewind(in);
 
    out = fopen(argv[2], "w");
    if( out==NULL ){
@@ -48,47 +39,48 @@ int main(int argc, char * argv[]){
    }
     // An array of strings
     char **string_array;
+	string_array = malloc(line_count* sizeof(char*));
+	for(int i =0 ;i < line_count; i++){
+		string_array[i] = malloc(4096);
+		
+	}
 
-    string_array = malloc(line_count);
-    string_array = NULL;
-    for(int i = 0; i < line_count; i++){
-        string_array[i] = malloc(300 * sizeof(char));
-        string_array[i] = NULL;
-    }
+
     int count = 0;// for counting words/ indexing will also serve as data to the list
     List L = newList();
 
     // Read in the words and put them in array, also create a list of its indices.
-    while(fscanf(in_read, "%s", word) != EOF){
-        string_array[count] = word;
+    while(fgets(string_array[count], 4096, in)){
+        
+	string_array[count][strlen(string_array[count])-1] = '\0';
+//	append(L, count);
         if(length(L) ==0){ // append the element if list is empty
             append(L, count);
             count++;
             continue;
         }
         moveBack(L);// Set the cursor to the back of list
-        while(index(L)>=0 && strcmp(word,string_array[get(L)])<0){ /* loop backward until
+        while(index(L)>=0 && strcmp(string_array[count],string_array[get(L)])<0){ /* loop backward until
         word >= the element compared.*/
             movePrev(L);// move cursor back a step
         }
         if(index(L) < 0){// it means the word is smaller than the first ele, sp prepend
             prepend(L, count);
         }
-        if(index(L)>=0 && strcmp(word, string_array[get(L)]>=0)){ /* it means the current
-            cursor element is greater than the new element.*/
+        if(index(L)>=0 && strcmp(string_array[count], string_array[get(L)])>=0){ /* it means the current
+           cursor element is greater than the new element.*/
             insertAfter(L, count);//insert after the cursor.
         } 
         count ++;
     }
+    
 
     // Print the input in a orderly manner 
 
-    moveFront(L);
-    while(index(L)>=0 && index(L)< length(L)){
+    for(moveFront(L); index(L) >= 0; moveNext(L)){
         fprintf(out,"%s\n", string_array[get(L)]);
-        moveNext(L);
     }
-    fclose(out);
+    
 
     // free the string array
     for(int i = 0; i < line_count; i++){
@@ -97,5 +89,10 @@ int main(int argc, char * argv[]){
     free(string_array);
     // free the list
     freeList(&L);
-      
+
+// close the files
+ fclose(in);    
+ fclose(out);
 }
+
+
