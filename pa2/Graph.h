16413c0f6b6f112db22a9ff5b49f836f1d76d669
Tanishq Patil <tmpatil@ucsc.edu>
2021-01-23 06:55:01 -0800

    Upload New File

---
+++ pa2/Graph.h
@@ -0,0 +1,24 @@
+#ifndef GRAPH_H_
+#define GRAPH_H_
+
+//Grpah.h 
+typedef struct *GraphObj Graph;
+/*** Constructors-Destructors ***/
+Graph newGraph(int n);
+void freeGraph(Graph* pG);
+/*** Access functions ***/
+int getOrder(Graph G);
+int getSize(Graph G);
+int getSource(Graph G);
+int getParent(Graph G, int u);
+int getDist(Graph G, int u);
+void getPath(List L, Graph G, int u);
+/*** Manipulation procedures ***/
+void makeNull(Graph G);
+void addEdge(Graph G, int u, int v);
+void addArc(Graph G, int u, int v);
+void BFS(Graph G, int s);
+/*** Other operations ***/
+void printGraph(FILE* out, Graph G);
+
+#endif
