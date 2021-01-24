234ac6ac4eb0f7c1f7b6aeabd7869222cceff48a
Tanishq Patil <tmpatil@ucsc.edu>
2021-01-24 14:03:47 -0800

    Passes 6/10 test cases

--- pa2/Graph.c
+++ pa2/Graph.c
@@ -33,9 +33,10 @@ Graph newGraph(int n){
     G->distance = calloc(n+1, sizeof(int));
     for(int i = 1; i <= n; i++){
         G->Array[i] = newList();
-        G->colors[i] = W;
+	G->colors[i] = W;
         G->parent[i] = NIL;
-       G->distance[i] = INF;
+        G->distance[i] = INF;
+
         }
 
     G->vertices = n;
@@ -63,106 +64,119 @@ int getOrder(Graph G){
 }
 
 int getSize(Graph G){
-    int tot= 0;
-    for(int i = 0; i <= getOrder(G);i++){
-        tot = tot + length(G->Array[i]);
-    }
-    G->size = tot/2;
+  
     return G->size;
 }
 int getSource(Graph G){
+   if(G == NULL){
+	printf("G is null: source\n");
+	exit(EXIT_FAILURE);
+	}
+   if(G->source == NIL){
+	return NIL;
+	}
     return G->source;
 }
 int getParent(Graph G, int u){
-    if( u <= 0 || u > getOrder(G)){
-        printf("u is invalid\n");
-        exit(EXIT_FAILURE);
-    }
-    if(getSource(G) == NIL){
+   if(G == NULL){
+	printf(" G is null getparent\n");
+	exit(EXIT_FAILURE);
+	}
+   if( u < 1 || u >getOrder(G)){
+	printf("u is invalid at get parent\n");
+	exit(EXIT_FAILURE);
+	}
+   if(getSource(G) == NIL){
         return NIL;
     }
+   if(G->parent[u] == NIL){
+	return NIL;
+	}
         return G->parent[u];
 }
 int getDist(Graph G, int u){
-    if( u <= 0 || u > getOrder(G)){
-        printf("u is invalid\n");
-        exit(EXIT_FAILURE);
-    }
-    if(getSource(G)!= NIL){
-        return G->distance[u];
-    }
-    return INF;
-}
+   if(G == NULL){
+	printf(" Graph is not valid: get dist\n");
+	exit(EXIT_FAILURE);
+	}
+   if(u < 1 || u >getOrder(G)){
+	printf(" u is invalid: get dist\n");
+	exit(EXIT_FAILURE);
+	} 
+      if(getSource(G) == NIL){
+        return INF;
+        }
+       return G->distance[u];
+      }
+   
+
 void getPath(List L, Graph G, int u){
-    if( u <= 0 || u > getOrder(G)){
-        printf("u is invalid\n");
-        exit(EXIT_FAILURE);
-    }
+   if(G == NULL){
+	printf("Gis invalid: getPath\n");
+	exit(EXIT_FAILURE);
+	}
+    if(u < 1 || u > getOrder(G)){
+	printf("u is invalid: getpath\n");
+	exit(EXIT_FAILURE);
+	} 
     if (getSource(G) == NIL){
         printf("BFS not run!\n");
         exit(EXIT_FAILURE);
     }
     if(u == getSource(G)){
         append(L,u);
-        return;
+       
     }
     else if(G->parent[u] == NIL){
-        append(L,NIL);
+        append(L, NIL);
     }
-   
+    else{
+	getPath(L,G,G->parent[u]);
+	append(L, u);
+	}
 }
+
 /*** Manipulation procedures ***/
 void makeNull(Graph G){
     for(int i = 1; i <= getOrder(G); i++){
         clear(G->Array[i]);
         free(G->Array[i]);
-
+	G->colors[i] = W;
+	G->distance[i] = INF;
+	G->parent[i] = NIL;
     }
 }
 void addEdge(Graph G, int u, int v){
-    if(length(G->Array[u])!=0){
+
+        if(length(G->Array[u])!=0){
 
         for(moveFront(G->Array[u]);index(G->Array[u]) < length(G->Array[u]);moveNext(G->Array[u])){
-                if(index(G->Array[u]) < 0){
+        if(index(G->Array[u]) < 0){
                         printf("%d\n", index(G->Array[u]));
                         exit(EXIT_FAILURE);
-                }
+	}
 
             if(get(G->Array[u]) > v){
                 insertBefore(G->Array[u],v);
+               
                 break;
             }
-            if(index(G->Array[u]) == (length(G->Array[u])-1)){
+            else if(index(G->Array[u]) == (length(G->Array[u])-1)){
                 append(G->Array[u], v);
+               
                 break;
                 }
         }
     }
     if(length(G->Array[u]) == 0){
         append(G->Array[u], v);
+        
     }
-    if(length(G->Array[v])!=0){
-
-        for(moveFront(G->Array[v]);index(G->Array[v]) < length(G->Array[v]);moveNext(G->Array[v])){
-                if(index(G->Array[v]) < 0){
-                        printf("%d\n", index(G->Array[v]));
-                       exit(EXIT_FAILURE);
-                }
-
-                if(get(G->Array[v]) > u){
-                insertBefore(G->Array[v], u);
-                break;
-            }
-            if(index(G->Array[v]) == (length(G->Array[v])-1)){
-                append(G->Array[v], u);
-                break;
-                }
-        }
-    }
-    if(length(G->Array[v]) == 0){
-        append(G->Array[v], u);
-    }
+	
+	addArc(G, v, u);
 }
+        
+
 void addArc(Graph G, int u, int v){
     if(length(G->Array[u])!=0){
 
@@ -174,51 +188,58 @@ void addArc(Graph G, int u, int v){
 
             if(get(G->Array[u]) > v){
                 insertBefore(G->Array[u],v);
+		G->size++;
                 break;
             }
-            if(index(G->Array[u]) == (length(G->Array[u])-1)){
+            else if(index(G->Array[u]) == (length(G->Array[u])-1)){
                 append(G->Array[u], v);
+		G->size++;
                 break;
                 }
         }
     }
     if(length(G->Array[u]) == 0){
         append(G->Array[u], v);
+	G->size ++;
     }
 }
 void BFS(Graph G, int s){
-    G->distance[s] = 0;
-    G->parent[s] = NIL;
-    G->colors[s] = Gr;
-    List L;
-    L = newList();
-    append(L,s);
+  for(int i = 1; i <= getOrder(G); i++){
+       
+    if(i == s){
+   	 G->colors[i] = Gr;
+   	 G->parent[i] = NIL;
+   	 G->distance[i] = 0;
+	}
+	G->colors[i] = W;
+        G->parent[i] = NIL;
+        G->distance[i] = INF;
+
+  }
+    //G->source = s;
+    List L = newList();
+    append(L, s);
     moveFront(L);
-    while(length(L) >0){
-        if(index(L) < 0){
-         printf("error found\n");
-        exit(EXIT_FAILURE);
-        }
-        int x = get(L);
-        G->source = get(L);
-        deleteFront(L);
-        for(moveFront(G->Array[x]); index(G->Array[x]) >= 0 && index(G->Array[x]) < length(G->Array[x]);moveNext(G->Array[x])){
-                if(index(G->Array[x]) < 0){
-                 printf("error found\n");
-                exit(EXIT_FAILURE);
-                }
-
-
-            if(G->colors[get(G->Array[x])] == W){ // if cursor undiscovered 
-                G->colors[get(G->Array[x])] = Gr; // discover
-                G->distance[get(G->Array[x])] = G->distance[x] + 1;
-                G->parent[get(G->Array[x])] = x;
-                append(L,get(G->Array[x]));
-            }
-        }
-        G->colors[x] = B;
-        moveFront(L);
+    while(length(L) > 0){
+	int x = get(L); // store the cursors/source's  value
+	G->source = get(L);
+	deleteFront(L); // the line up and this act similar to dequeue
+	moveFront(G->Array[x]);// set the cursor at front
+	while(index(G->Array[x]) >= 0){
+		int u = get(G->Array[x]); //store the cursor value at u
+		if(G->colors[u] == W){
+			G->colors[u] = Gr;
+			G->distance[u] = G->distance[x] +1;
+			G->parent[u] = x;
+			append(L, x);
+		}
+		moveNext(G->Array[x]);
+	}
+	G->colors[x] = B;
+	moveFront(L);
     }
+   freeList(&L);
+   	
 }
 
 /*** Other operations ***/
