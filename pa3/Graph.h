eab461970e63c62e4f31b2d1fe6883f993da5fb9
Tanishq Patil <tmpatil@ucsc.edu>
2021-01-30 02:33:10 -0800

    Upload New File

---
+++ pa3/Graph.h
@@ -0,0 +1,48 @@
+/*********************************************************************************
+ *  * * Tanishq Patil, tmpatil
+ *  * * 2021 Winter CSE 101 pa3
+ *  * * Graph.h
+ *  * * header for Graph ADT
+ *  * *********************************************************************************/
+
+//-----------------------------------------------------------------------------
+//// Graph.h
+
+////-----------------------------------------------------------------------------
+#ifndef GRAPH_H_
+#define GRAPH_H_
+#include "List.h"
+#include<stdio.h>
+#define NIL -1
+#define INF -2
+#define UNDEF -3
+#define W 0
+#define Gr 1
+#define B 2
+
+//Grpah.h 
+typedef struct GraphObj* Graph;
+/*** Constructors-Destructors ***/
+Graph newGraph(int n);
+void freeGraph(Graph* pG);
+/*** Access functions ***/
+int getOrder(Graph G);
+int getSize(Graph G);
+int getParent(Graph G, int u); /* Pre: 1<=u<=n=getOrder(G) */
+int getDiscover(Graph G, int u); /* Pre: 1<=u<=n=getOrder(G) */
+int getFinish(Graph G, int u); /* Pre: 1<=u<=n=getOrder(G) */
+int getSource(Graph G);
+int getParent(Graph G, int u);
+int getDist(Graph G, int u);
+void getPath(List L, Graph G, int u);
+/*** Manipulation procedures ***/
+void makeNull(Graph G);
+void addEdge(Graph G, int u, int v);
+void addArc(Graph G, int u, int v);
+void BFS(Graph G, int s);
+void DFS(Graph G, List S); /* Pre: length(S)==getOrder(G) */
+/*** Other operations ***/
+Graph transpose(Graph G);
+Graph copyGraph(Graph G);
+void printGraph(FILE* out, Graph G);
+#endif
