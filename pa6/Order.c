05de6eb342f9da59e6fe5bba1c88012158c6beff
Tanishq Patil <tmpatil@ucsc.edu>
2021-02-25 10:55:14 -0800

    Order.c works with no leaks, but has valgrind errors

--- pa6/Order.c
+++ pa6/Order.c
@@ -1,98 +1,79 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<ctype.h>
-#include"Dictionary.h"
-
-#define MAX_LINE_LEN 300
+#include "Dictionary.h"
 
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]){
 
-   Dictionary D = newDictionary(1); // unique keys
-   FILE* in = NULL;
-   FILE* out = NULL;
-   char delim[] = "\x20\n\t\r\\\"\',<.>/?;:[{]}|`~!@#$^&*()-_=+0123456789"\
-                  "\xE2\x80\x98\x94\x99\x9C\x9D\x25";
-   char line[MAX_LINE_LEN];
-   char** keyBuffer = NULL;
-   int**  valBuffer = NULL;
-   char* word;
-   char* ch;
-   int* p;
-   int i, n=0;
+    int line_count =0;
+    char ch;
+    FILE *in;
+    FILE *out;
 
-   // check command line arguments 
-   if( argc != 3 ){
-      printf("Usage: %s <input file> <output file>\n", argv[0]);
-      exit(EXIT_FAILURE);
-   }
+if( argc != 3 ){
+        printf("Usage: %s <input file> <output file>\n", argv[0]);
+        exit(1);
+    }
 
-   // open input file 
-   in = fopen(argv[1], "r");
+  in = fopen(argv[1], "r");
       if( in==NULL ){
-      printf("Unable to open file %s for reading\n", argv[1]);
+      printf(" 1 Unable to open file %s for reading\n", argv[1]);
       exit(1);
    }
 
-   // open output file
+    while((ch=fgetc(in))!=EOF) {
+        if(ch=='\n'){
+		
+            line_count++;
+        }
+  }
+ rewind(in);
+
    out = fopen(argv[2], "w");
    if( out==NULL ){
       printf("Unable to open file %s for writing\n", argv[2]);
       exit(1);
    }
 
-   // read each line of input 
-   while( fgets(line, MAX_LINE_LEN, in) != NULL)  {
-      
-      // get each word on this line
-      word = strtok(line, delim);
-      while( word!=NULL ){ // there is another word
-         
-         // convert word to lower case
-         for(ch=word; *ch!='\0'; ch++){
-            *ch = tolower(*ch);
-         }
 
-         p = lookup(D, word);
-         if( p==VAL_UNDEF ){ // this word is new
-            n++;
+    char **string_array;
+	string_array = malloc(line_count* sizeof(char*));
+	for(int i =0 ;i < line_count; i++){
+		string_array[i] = malloc(4096);
+		
+	}
 
-            keyBuffer = realloc(keyBuffer, n*sizeof(KEY_TYPE));
-            keyBuffer[n-1] = calloc(strlen(word)+1, sizeof(char));
-            strcpy(keyBuffer[n-1], word);
+ int count = 0;// for counting words/ indexing will also serve as data to the list
+ 
+ Dictionary D = newDictionary(0); 
 
-            valBuffer = realloc(valBuffer, n*sizeof(VAL_TYPE));
-            valBuffer[n-1] = malloc(sizeof(int));
-            *valBuffer[n-1] = 1;
+int* p, c;
+ c = -1;
+ p   = &c;
+     while(fgets(string_array[count], 4096, in)){
+        
+    	string_array[count][strlen(string_array[count])-1] = '\0';
 
-            insert( D, keyBuffer[n-1], valBuffer[n-1] );
-
-         }else{ // this word has been seen already
-            (*p)++;
-         }
-
-         // get next token
-         word = strtok(NULL, delim);
-      }
-   }
-
- // Pre-order------------------------------------------------------------------
-
+        
+        insert(D,string_array[count], p);
+        count++;
+ 
+    }
     fprintf(out, "******************************************************\n");
     fprintf(out, "PRE-ORDER:\n");
     fprintf(out, "******************************************************\n");
 
     printDictionary(out, D, "pre");
     fprintf(out, "\n\n");
- //  IN-order --------------------------------------------------------------------
+ 
     fprintf(out, "******************************************************\n");
     fprintf(out, "IN-ORDER:\n");
     fprintf(out, "******************************************************\n");
 
     printDictionary(out, D, "in");
     fprintf(out, "\n\n");
- // POST-order --------------------------------------------------------------------
+ 
 
     fprintf(out, "******************************************************\n");
     fprintf(out, "POST-ORDER:\n");
@@ -100,17 +81,19 @@ int main(int argc, char* argv[]){
 
     printDictionary(out, D, "post");
     fprintf(out, "\n\n");
+  
 
-   // free resources
-   freeDictionary(&D);
-   for(i=0; i<n; i++){
-      free(keyBuffer[i]);
-      free(valBuffer[i]);
-   }
-   free(keyBuffer);
-   free(valBuffer);
-   fclose(in);
-   fclose(out);
+    // free the string array
+  for(int i = 0; i < line_count; i++){
+        free(string_array[i]);
+    }
+    free(string_array);
+   
+    freeDictionary(&D);
 
-   return(0);
+ fclose(in);    
+ fclose(out);
 }
+
+
+
